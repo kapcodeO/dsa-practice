d56feace17bc44f881c735a2638bd5f0c08ea365
kapcodeO <kapilojha953@gmail.com>
2025-07-12 19:46:33 +0530

    easy

---
+++ array/easy/longest-commom-prefix.py
@@ -0,0 +1,15 @@
+# leetcode 14: Longest Common Prefix
+# Input: strs = ["flower","flow","flight"]
+# Output: "fl"
+
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        pass        
+
+test = Solution()
+
+strs = ["flower","flow","flight"]
+print(test.longestCommonPrefix(strs)
+
+strs = ["dog","racecar","car"]
+print(test.longestCommonPrefix(strs)
