fcd4e68a1392804a13e1e7e4df50a055fe3945e8
kapcodeO <kapilojha953@gmail.com>
2025-09-03 01:17:45 +0530

    add folder

--- array/temp.py
+++
@@ -1,9 +0,0 @@
-strs = ["bat", "bag", "bank", "bad"]
-
-idx = 0
-while idx < len(strs):
-    for i in range(len(strs[0])):
-        if i < len(strs[i]):
-            print(strs[i][idx], end=" ")
-    print()
-    idx += 1
